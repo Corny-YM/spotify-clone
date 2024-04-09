17e420384334ced45aa96c7dd8ccd0b2671effdb
theanh1508 <85997901+0862551396@users.noreply.github.com>
2024-04-09 23:34:47 +0700

    add env NEXT_PUBLIC_SITE_URL

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -1,10 +1,7 @@
 import { Price } from "@/types/common";
 
 export const getUrl = () => {
-  let url =
-    process.env.NEXT_PUBLIC_SITE_URL ??
-    process.env.NEXT_PUBLIC_VERCEL_URL ??
-    "http://localhost:3000/";
+  let url = process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000/";
 
   url = url.includes("http") ? url : `https://${url}`;
   url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
